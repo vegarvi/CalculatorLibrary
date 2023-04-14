a0504e8c65db84755383cb02e97fe4686d4d0684
Vegard Vinje <vegard@simula.no>
2023-04-14 13:54:36 +0200

    added multiplication test

--- test_calculator.py
+++ test_calculator.py
@@ -10,3 +10,6 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 4 == calculator.multiply(2, 2)
